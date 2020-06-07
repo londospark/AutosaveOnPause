66f8d127ec333bc043be85bd19785cb20051775b
Gareth Hubball <ridecar2@gmail.com>
2020-06-07 22:31:31 +0100

    Added debug configuration

--- Src/AutosaveOnPause/AutosaveOnPause.cs
+++ Src/AutosaveOnPause/AutosaveOnPause.cs
@@ -3,8 +3,43 @@ using UnityEngine;
 
 namespace AutosaveOnPause
 {
-    public class AutosaveOnPause: IUserMod {
-		public string Name => "Autosave on Pause";
-		public string Description => "Save your game everytime that you pause it.";
-	}
+
+    public static class Constants
+    {
+        public const string DefaultFileName = "AutoSavedOnPause";
+    }
+    public class AutosaveOnPause : IUserMod
+    {
+        public string Name => "Autosave on Pause";
+        public string Description => "Save your game everytime that you pause it.";
+
+        public void OnSettingsUI(UIHelperBase helper)
+        {
+            var group = helper.AddGroup("Save options:");
+            group.AddTextfield("Save name", Constants.DefaultFileName, value => Mod.SaveName = value);
+        }
+    }
+
+    public class Mod : ThreadingExtensionBase
+    {
+        public static string SaveName { get; set; } = Constants.DefaultFileName;
+        private bool savedThisPause = false;
+        private ISerializableData serializableData;
+
+        public override void OnCreated(IThreading threading)
+        {
+            serializableData = threading.managers.serializableData;
+        }
+        public override void OnAfterSimulationTick()
+        {
+            if (threadingManager.simulationPaused != true && !savedThisPause)
+            {
+                savedThisPause = true;
+                serializableData.SaveGame(SaveName);
+            }
+
+            if (!threadingManager.simulationPaused == true)
+                savedThisPause = false;
+        }
+    }
 }
