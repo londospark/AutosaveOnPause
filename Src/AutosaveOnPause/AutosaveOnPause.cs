c810b8e6eefa1604fe490a554eb148639cbc60c0
Gareth Hubball <ridecar2@gmail.com>
2020-06-07 18:57:50 +0100

    Initial Commit

---
+++ Src/AutosaveOnPause/AutosaveOnPause.cs
@@ -0,0 +1,10 @@
+﻿using ICities;
+using UnityEngine;
+
+namespace AutosaveOnPause
+{
+    public class AutosaveOnPause: IUserMod {
+		public string Name => "Autosave on Pause";
+		public string Description => "Save your game everytime that you pause it.";
+	}
+}
---
+++ build.fsx
@@ -0,0 +1,45 @@
+// include Fake lib
+#r "paket:
+nuget Fake.IO.FileSystem
+nuget Fake.Core.Target
+nuget Fake.DotNet.MSBuild //"
+
+#if !FAKE
+#load "./.fake/build.fsx/intellisense.fsx"
+#endif
+
+open Fake.Core
+open Fake.DotNet
+open Fake.IO.Globbing.Operators
+open Fake.IO
+
+let buildDir = "./Build/"
+
+let installFolder = (Environment.environVar "LOCALAPPDATA") + "/Colossal Order/Cities_Skylines/Addons/Mods/AutosaveOnPause"
+
+Target.create "Clean" (fun _ ->
+    Shell.cleanDir buildDir
+)
+
+Target.create "Build" (fun _ -> 
+    !! "./Src/**/*.csproj"
+      |> MSBuild.runRelease id buildDir "Build"
+      |> Trace.logItems "AppBuild-Output: "
+)
+
+Target.create "Install" (fun _ -> 
+    Shell.mkdir (installFolder)
+    Shell.copyFile installFolder (buildDir + "AutosaveOnPause.dll")
+)
+
+open Fake.Core.TargetOperators
+
+// Dependencies
+"Clean"
+  ==> "Build"
+
+"Build"
+  ==> "Install"
+
+// start build
+Target.runOrDefault "Install"
